23a9e4a2a87b3d8d60ad0800c902cc232662b849
Lukas_Soucek <lukas.soucek@tul.cz>
2018-04-26 07:17:12 +0200

    testicek

---
+++ polygon/Point.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package polygon;
+
+/**
+ *
+ * @author lukas.soucek
+ */
+public class Point {
+
+    @Override // tato metoda existuje, tak to použije 
+    public String toString() {
+        return "Hodnota " + "x" + x + ", hodnota y" + y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+   public double getDistance()
+   {
+       return Math.sqrt(x*x + y*y);
+   }
+   public double getDistance(Point p) // přetěžování metody
+   {
+     double distX = (x-p.x);
+     double distY = (y-p.y);
+     return Math.sqrt(distX*distX+distY*distY);
+   }
+    public static void main(String[] args) {
+        Point p = new Point(4, 4);
+        System.out.println(p);
+        System.out.println(p.getX() + " " + p.getY()) ;
+        System.out.println(p.getDistance());
+        System.out.println(p.getDistance(p));
+    }
+}
