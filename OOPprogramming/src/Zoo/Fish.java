44bcb799d2c4f0eafc25679226c3aae84eedb099
Lukas_Soucek <lukas.soucek@tul.cz>
2018-04-26 11:52:49 +0200

    dfd

---
+++ OOPprogramming/src/Zoo/Fish.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Zoo;
+
+/**
+ *
+ * @author SULO
+ */
+public class Fish {
+
+    
+     
+public void swim()
+{
+    System.out.println("I am swimming");
+}
+    
+    
+}
